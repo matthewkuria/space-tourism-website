e4a8bcae8058ff7e0c7ecb74cef6239f2532b186
matthewkuria <mathewkwachira@gmail.com>
2024-08-26 23:09:28 +0300

    Add the moon image

--- src/routes/destination.jsx
+++ src/routes/destination.jsx
@@ -1,24 +1,82 @@
-import { Tab } from '@mui/base/Tab';
+import * as React from 'react';
+import { Tabs } from '@mui/base/Tabs';
 import { TabsList } from '@mui/base/TabsList';
 import { TabPanel } from '@mui/base/TabPanel';
-import { Tabs } from '@mui/base/Tabs';
+import { Tab } from '@mui/base/Tab';
+import imageMoon from "../assets/destination/image-moon.png" 
 
 
 
-import Button from '@material-ui/core/Button';
 const Destination = () => {
     return (
         <main className="text-white">
             <h1 className="uppercase text-2xl"><span className="text-slate-500">01</span> Pick your destination</h1>
             <div className="">
                 <div className="">
-                
+                  <img src={imageMoon} alt="The moon image" />
                 </div>
                 <div className="">
-                        <Button variant="contained" color="primary" 
-                                size="large">
-                            GeeksforGeeks
-                        </Button>
+                         <Tabs defaultValue={1}>
+                            <TabsList className="mb-4 rounded-xl bg-purple-500 flex font-sans items-center justify-center content-between min-w-tabs-list shadow-lg">
+                                <Tab
+                                slotProps={{
+                                    root: ({ selected, disabled }) => ({
+                                    className: `font-sans ${
+                                        selected
+                                        ? 'text-purple-500 bg-white'
+                                        : 'text-white bg-transparent focus:text-white hover:bg-purple-400'
+                                    } ${
+                                        disabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'
+                                    } text-sm font-bold w-full p-2 m-1.5 border-0 rounded-lg flex justify-center focus:outline-0 focus:shadow-outline-purple-light`,
+                                    }),
+                                }}
+                                value={1}
+                                >
+                                One
+                                </Tab>
+                                <Tab
+                                slotProps={{
+                                    root: ({ selected, disabled }) => ({
+                                    className: `font-sans ${
+                                        selected
+                                        ? 'text-purple-500 bg-white'
+                                        : 'text-white bg-transparent focus:text-white hover:bg-purple-400'
+                                    } ${
+                                        disabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'
+                                    } text-sm font-bold w-full p-2 m-1.5 border-0 rounded-md flex justify-center focus:outline-0 focus:shadow-outline-purple-light`,
+                                    }),
+                                }}
+                                value={2}
+                                >
+                                Two
+                                </Tab>
+                                <Tab
+                                slotProps={{
+                                    root: ({ selected, disabled }) => ({
+                                    className: `font-sans ${
+                                        selected
+                                        ? 'text-purple-500 bg-white'
+                                        : 'text-white bg-transparent focus:text-white hover:bg-purple-400'
+                                    } ${
+                                        disabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'
+                                    } text-sm font-bold w-full p-2 m-1.5 border-0 rounded-md flex justify-center focus:outline-0 focus:shadow-outline-purple-light`,
+                                    }),
+                                }}
+                                value={3}
+                                >
+                                Three
+                                </Tab>
+                            </TabsList>
+                            <TabPanel className="w-full font-sans text-sm" value={1}>
+                                First page
+                            </TabPanel>
+                            <TabPanel className="w-full font-sans text-sm" value={2}>
+                                Second page
+                            </TabPanel>
+                            <TabPanel className="w-full font-sans text-sm" value={3}>
+                                Third page
+                            </TabPanel>
+                            </Tabs>
                 </div>
             </div>
         </main>
