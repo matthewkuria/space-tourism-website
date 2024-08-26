5159bfa43c6c08f34cc5839742d8c3e6340855c9
matthewkuria <mathewkwachira@gmail.com>
2024-08-26 22:54:44 +0300

    Add Material UI

--- src/routes/destination.jsx
+++ src/routes/destination.jsx
@@ -1,8 +1,28 @@
+import { Tab } from '@mui/base/Tab';
+import { TabsList } from '@mui/base/TabsList';
+import { TabPanel } from '@mui/base/TabPanel';
+import { Tabs } from '@mui/base/Tabs';
+
+
+
+import Button from '@material-ui/core/Button';
 const Destination = () => {
     return (
         <main className="text-white">
-            <h1>This is the destination page</h1>
+            <h1 className="uppercase text-2xl"><span className="text-slate-500">01</span> Pick your destination</h1>
+            <div className="">
+                <div className="">
+                
+                </div>
+                <div className="">
+                        <Button variant="contained" color="primary" 
+                                size="large">
+                            GeeksforGeeks
+                        </Button>
+                </div>
+            </div>
         </main>
     )
 }
 export default Destination
+
