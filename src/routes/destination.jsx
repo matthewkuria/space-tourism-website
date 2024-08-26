91eee1d159036089981d4fa19c6ab63e8c00d7cb
matthewkuria <mathewkwachira@gmail.com>
2024-08-26 23:24:38 +0300

    add the four tabs

--- src/routes/destination.jsx
+++ src/routes/destination.jsx
@@ -9,15 +9,15 @@ import imageMoon from "../assets/destination/image-moon.png"
 
 const Destination = () => {
     return (
-        <main className="text-white">
+        <main className="text-white p-24">
             <h1 className="uppercase text-2xl"><span className="text-slate-500">01</span> Pick your destination</h1>
-            <div className="">
+            <div className="flex justify-around items-start">
                 <div className="">
                   <img src={imageMoon} alt="The moon image" />
                 </div>
                 <div className="">
                          <Tabs defaultValue={1}>
-                            <TabsList className="mb-4 rounded-xl bg-purple-500 flex font-sans items-center justify-center content-between min-w-tabs-list shadow-lg">
+                            <TabsList className="mb-4 rounded-xl bg-transparent flex font-sans items-center justify-center content-between min-w-tabs-list shadow-lg">
                                 <Tab
                                 slotProps={{
                                     root: ({ selected, disabled }) => ({
@@ -32,7 +32,7 @@ const Destination = () => {
                                 }}
                                 value={1}
                                 >
-                                One
+                                Moon
                                 </Tab>
                                 <Tab
                                 slotProps={{
@@ -48,7 +48,7 @@ const Destination = () => {
                                 }}
                                 value={2}
                                 >
-                                Two
+                                Mars
                                 </Tab>
                                 <Tab
                                 slotProps={{
@@ -64,7 +64,23 @@ const Destination = () => {
                                 }}
                                 value={3}
                                 >
-                                Three
+                               Europa
+                            </Tab>
+                            <Tab
+                                slotProps={{
+                                    root: ({ selected, disabled }) => ({
+                                    className: `font-sans ${
+                                        selected
+                                        ? 'text-purple-500 bg-white'
+                                        : 'text-white bg-transparent focus:text-white hover:bg-purple-400'
+                                    } ${
+                                        disabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'
+                                    } text-sm font-bold w-full p-2 m-1.5 border-0 rounded-md flex justify-center focus:outline-0 focus:shadow-outline-purple-light`,
+                                    }),
+                                }}
+                                value={4}
+                                >
+                               Titan
                                 </Tab>
                             </TabsList>
                             <TabPanel className="w-full font-sans text-sm" value={1}>
@@ -75,6 +91,9 @@ const Destination = () => {
                             </TabPanel>
                             <TabPanel className="w-full font-sans text-sm" value={3}>
                                 Third page
+                            </TabPanel>
+                            <TabPanel className="w-full font-sans text-sm" value={4}>
+                                Fourth Page
                             </TabPanel>
                             </Tabs>
                 </div>
