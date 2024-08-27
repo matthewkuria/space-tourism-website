4d75e54e80edf407921485d28383a2ff1a8671eb
matthewkuria <mathewkwachira@gmail.com>
2024-08-27 20:19:53 +0300

    Fix the material UI issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles';
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/Navbar";
 
+const theme = createMuiTheme();
+
 export default function App() {
   return (
-    <main className="HOME relative">
+    <ThemeProvider theme={theme}>
+      <main className="HOME relative">
       <NavBar />
       <Outlet />
     </main>
+    </ThemeProvider>
   )
 }
