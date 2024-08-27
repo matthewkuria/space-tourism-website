476ad3c537e6affbc9cc6a5f48a583935a59a2a6
matthewkuria <mathewkwachira@gmail.com>
2024-08-27 10:43:37 +0300

    Add the main moon component

--- src/routes/destination.jsx
+++ src/routes/destination.jsx
@@ -9,13 +9,13 @@ import imageMoon from "../assets/destination/image-moon.png"
 
 const Destination = () => {
     return (
-        <main className="text-white p-24">
+        <main className="destination text-white p-12">
             <h1 className="uppercase text-2xl"><span className="text-slate-500">01</span> Pick your destination</h1>
             <div className="flex justify-around items-start">
                 <div className="">
                   <img src={imageMoon} alt="The moon image" />
                 </div>
-                <div className="">
+                <div className="w-full md:w-1/2">
                          <Tabs defaultValue={1}>
                             <TabsList className="mb-4 rounded-xl bg-transparent flex font-sans items-center justify-center content-between min-w-tabs-list shadow-lg">
                                 <Tab
@@ -83,8 +83,35 @@ const Destination = () => {
                                Titan
                                 </Tab>
                             </TabsList>
-                            <TabPanel className="w-full font-sans text-sm" value={1}>
-                                <h1 className='uppercase text-3xl'>Moon</h1>
+                            <TabPanel className="w-full font-sans text-sm flex flex-col" value={1}>
+                                <h1 className='uppercase text-8xl text-center'>Moon</h1>
+                                <div className="">
+                                    <p className="font-bold py-6">
+                                        See our planet as you have never seen it before.
+                                        A perfect relaxing trip away to help regain perspective and come back refreshed.
+                                        While you are there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
+                                    </p>
+                            </div>
+                            <div className="pt-6">
+                                <div className="flex  justify-between">
+                                    <div className="">
+                                        <p className="uppercase">
+                                        Avg. distance 
+                                        </p>
+                                        <p className="text-xl">
+                                            384,400 km
+                                        </p>
+                                    </div>
+                                    <div className="">
+                                          <p className="uppercase">
+                                        Est. travel time
+                                        </p>
+                                        <p className="text-xl">
+                                            3 days
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                             </TabPanel>
                             <TabPanel className="w-full font-sans text-sm" value={2}>
                                 Second page
