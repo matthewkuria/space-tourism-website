352dd3ab91a514d10711884fb700dc5e8bc38e58
matthewkuria <mathewkwachira@gmail.com>
2024-08-30 00:00:08 +0300

    Add react tabs with mapped data.json items

--- src/routes/destination.jsx
+++ src/routes/destination.jsx
@@ -1,6 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import imageMoon from "../assets/destination/image-moon.png"
+import imageMars from "../assets/destination/image-mars.png"
+import imageEuro from "../assets/destination/image-europa.png"
+import imageTitan from "../assets/destination/image-titan.png"
+
+const imageMap = {
+    'image-moon.png': imageMoon,
+    'image-mars.png': imageMars,
+    'image-europa.png': imageEuro,
+    'image-titan.png': imageTitan
+    
+};
 const Destination = () => {
+    const [data, setData] = useState([])
+    const [value, setValue] = useState(0);
+    // Fetch data from the  JSON file
+     useEffect(() => {
+    // Fetch data from the JSON file
+    fetch('/data.json')
+      .then((response) => response.json())
+      .then((data) => setData(data.destinations))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    };
     return (
-        <h1>Destination Page Here</h1>
+        <section className='destination'>
+            <h1 className="uppercase text-white text-2xl py-10"><span className="text-slate-500">01</span> Pick your destination</h1>
+            <Tabs>
+            <TabList className="flex justify-end md:mr-28 uppercase md:pt-10">
+                {Array.isArray(data) && data.map((item, index) => (
+                <Tab key={index}>{item.name}</Tab>
+                ))}
+            </TabList>
+            {Array.isArray(data) && data.map((item, index) => (
+                <TabPanel key={index}>
+                <div className='flex flex-col md:flex-row justify-around'>
+                    <div className=''>
+                    <img
+                        src={imageMap[item.images.png]}
+                        alt={item.title}
+                        style={{ width: '100%', borderRadius: '8px' }}
+                    />
+                    </div>
+                    <div className="flex flex-col text-white w-1/2">
+                        <h2 className='uppercase text-7xl'>{item.name}</h2>
+                        <p>{item.description}</p>
+                        <div className="flex justify-between mt-10 ">
+                            <div className="flex flex-col">
+                                <p className="">AVG. DISTANCE</p>
+                                <p>{item.distance}</p>
+                            </div>
+                            <div className="flex flex-col">
+                                <p className="">EST. TRAVEL TIME</p>
+                                <p>{ item.travel}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </TabPanel>
+            ))}
+            </Tabs>
+        </section>
     )
 }
 export default Destination
--- src/routes/destinationold.jsx
+++ src/routes/destinationold.jsx
@@ -1,83 +1,83 @@
-// import React, { useState, useEffect } from 'react';
-// import { Tabs, Tab, Box, Typography } from '@mui/material';
-// import imageMoon from "../assets/destination/image-moon.png"
-// import imageMars from "../assets/destination/image-mars.png"
-// import imageEuro from "../assets/destination/image-europa.png"
-// import imageTitan from "../assets/destination/image-titan.png"
+import React, { useState, useEffect } from 'react';
+import { Tabs, Tab, Box, Typography } from '@mui/material';
+import imageMoon from "../assets/destination/image-moon.png"
+import imageMars from "../assets/destination/image-mars.png"
+import imageEuro from "../assets/destination/image-europa.png"
+import imageTitan from "../assets/destination/image-titan.png"
 
-// const imageMap = {
-//     'image-moon.png': imageMoon,
-//     'image-mars.png': imageMars,
-//     'image-europa.png': imageEuro,
-//     'image-titan.png': imageTitan
+const imageMap = {
+    'image-moon.png': imageMoon,
+    'image-mars.png': imageMars,
+    'image-europa.png': imageEuro,
+    'image-titan.png': imageTitan
     
-// };
-// const Destination = () => {
-//     const [data, setData] = useState([])
-//     const [value, setValue] = useState(0);
-//     // Fetch data from the  JSON file
-//      useEffect(() => {
-//     // Fetch data from the JSON file
-//     fetch('/data.json')
-//       .then((response) => response.json())
-//       .then((data) => setData(data.destinations))
-//       .catch((error) => console.error('Error fetching data:', error));
-//   }, []);
+};
+const Destination = () => {
+    const [data, setData] = useState([])
+    const [value, setValue] = useState(0);
+    // Fetch data from the  JSON file
+     useEffect(() => {
+    // Fetch data from the JSON file
+    fetch('/data.json')
+      .then((response) => response.json())
+      .then((data) => setData(data.destinations))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
 
-//   const handleChange = (event, newValue) => {
-//     setValue(newValue);
-//     };
-//     const TabPanel = ({ children, value, index }) => {
-//         return (
-//             <div role="tabpanel" hidden={value !== index}>
-//             {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-//             </div>
-//         );
-// };
-//     return (
-//         <main className="destination text-white p-12">
-//             <h1 className="uppercase text-2xl"><span className="text-slate-500">01</span> Pick your destination</h1>
-//             <div className="">
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    };
+    const TabPanel = ({ children, value, index }) => {
+        return (
+            <div role="tabpanel" hidden={value !== index}>
+            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+            </div>
+        );
+};
+    return (
+        <main className="destination text-white p-12">
+            <h1 className="uppercase text-2xl"><span className="text-slate-500">01</span> Pick your destination</h1>
+            <div className="">
               
-//                 <div className="w-full flex justify-end ">
-//                     <Box sx={{ width: '100%' }}>
-//                         <Tabs className=' text-white' value={value} onChange={handleChange} aria-label="">
-//                             {Array.isArray(data) && data.map((item, index) => (
-//                             <Tab key={index} label={item.name} />
-//                             ))}
-//                         </Tabs>
-//                         {data.map((item, index) => (
-//                             <TabPanel key={index} value={value} index={index} className="">
-//                                 <div className="flex flex-col md:flex-row justify-around">
-//                                     <div className="">
-//                                     <img
-//                                     src={imageMap[item.images.png]}
-//                                     alt={item.title}
-//                                     style={{ width: '100%', borderRadius: '8px' }}
-//                                     />
-//                                     </div>
-//                                 <div className="w-full md:w-1/2">
-//                                     <Typography>{item.description}</Typography>
-//                                     <div className="flex justify-between mt-10 ">
-//                                         <div className="flex flex-col">
-//                                             <p className="">AVG. DISTANCE</p>
-//                                             <Typography>{item.distance}</Typography>
-//                                         </div>
-//                                         <div className="flex flex-col">
-//                                             <p className="">EST. TRAVEL TIME</p>
-//                                             <Typography>{ item.travel}</Typography>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 </div>
-//                             </TabPanel>
-//                         ))}
-//                     </Box>
-//                 </div>
-//             </div>
-//         </main>
-//     )
-// }
+                <div className="w-full flex justify-end ">
+                    <Box sx={{ width: '100%' }}>
+                        <Tabs className=' text-white' value={value} onChange={handleChange} aria-label="">
+                            {Array.isArray(data) && data.map((item, index) => (
+                            <Tab key={index} label={item.name} />
+                            ))}
+                        </Tabs>
+                        {data.map((item, index) => (
+                            <TabPanel key={index} value={value} index={index} className="">
+                                <div className="flex flex-col md:flex-row justify-around">
+                                    <div className="">
+                                    <img
+                                    src={imageMap[item.images.png]}
+                                    alt={item.title}
+                                    style={{ width: '100%', borderRadius: '8px' }}
+                                    />
+                                    </div>
+                                <div className="w-full md:w-1/2">
+                                    <Typography>{item.description}</Typography>
+                                    <div className="flex justify-between mt-10 ">
+                                        <div className="flex flex-col">
+                                            <p className="">AVG. DISTANCE</p>
+                                            <Typography>{item.distance}</Typography>
+                                        </div>
+                                        <div className="flex flex-col">
+                                            <p className="">EST. TRAVEL TIME</p>
+                                            <Typography>{ item.travel}</Typography>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </TabPanel>
+                        ))}
+                    </Box>
+                </div>
+            </div>
+        </main>
+    )
+}
 
-// export default Destination
+export default Destination
 
