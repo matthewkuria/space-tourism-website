8aed8114a9c32108bbf2e96d877f2d1219620538
matthewkuria <mathewkwachira@gmail.com>
2024-08-29 23:17:12 +0300

    Remove the ThemeProvider from the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles';
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/Navbar";
 
-const theme = createMuiTheme();
-
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
       <main className="HOME relative">
       <NavBar />
       <Outlet />
     </main>
-    </ThemeProvider>
   )
 }
