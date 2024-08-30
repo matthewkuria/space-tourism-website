1a9ddd52b22c9e15e98c0bc5e1dc4518cc4cbed2
matthewkuria <mathewkwachira@gmail.com>
2024-08-30 11:52:15 +0300

    Add crew background image

--- src/routes/crew.jsx
+++ src/routes/crew.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react";
+import "./routes.css";
 const Crew = () => {
+    const [data, setData] = useState([])
+    const [value, setValue] = useState(0);
+     // Fetch data from the  JSON file
+     useEffect(() => {
+    // Fetch data from the JSON file
+    fetch('/data.json')
+      .then((response) => response.json())
+      .then((data) => setData(data.destinations))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
     return (
-        <main className="text-white">
-            <h1>This is the Crew page</h1>
-        </main>
+        <div className="crew">
+            <h1 className="uppercase  text-white text-2xl pt-12 pb-0 pl-20">
+                <span className="text-slate-500">02</span>
+                Meet your crew
+            </h1>
+
+            
+       </div>
     )
 }
 export default Crew
