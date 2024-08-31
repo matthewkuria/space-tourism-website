6df197b8cf7eff790e4716ca525c99c42dd7255f
matthewkuria <mathewkwachira@gmail.com>
2024-08-31 19:10:13 +0300

    Rebuild the NavBar numbers

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,12 @@ const NavBar = () => {
             </div>
             <div className=" absolute left-32 right-0  h-[0.5px] bg-white w-1/3"></div>
             <div className="nav-items bg-transparent backdrop-blur-md w-2/3 px-10">
-                <ol className=" hidden md:flex lg:flex justify-around items-center uppercase list-decimal h-20 pr-10">
-                <li className=" border-b-2 hover:border-b-2 py-7 px-4"><a href={`/`}>home</a></li>
-                <li className="hover:border-b-2 py-7 px-4"><a href={`/destination`}>destination</a></li>
-                <li className="hover:border-b-2 py-7 px-4"><a href={`/crew`}>crew</a></li>
-                <li className="hover:border-b-2 py-7 px-4"><a href={`/technology`}>technology</a></li>
-            </ol>
+                <ul className=" hidden md:flex lg:flex justify-around items-center uppercase  h-20 pr-10">
+                    <li className=" border-b-2 hover:border-b-2 py-7 px-4"><a href={`/`}><span className=" font-bold px-2">01</span>home</a></li>
+                    <li className="hover:border-b-2 py-7 px-4"><a href={`/destination`}><span className=" font-bold px-2">02</span>destination</a></li>
+                    <li className="hover:border-b-2 py-7 px-4"><a href={`/crew`}><span className=" font-bold px-2">03</span>crew</a></li>
+                    <li className="hover:border-b-2 py-7 px-4"><a href={`/technology`}><span className=" font-bold px-2">04</span>technology</a></li>
+                </ul>
             </div>
             <div className="hamburger-icon flex md:hidden">
                 <img src={closebtn} alt="" />
