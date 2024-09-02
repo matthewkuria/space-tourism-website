7ccf8cdfa60b9897c8fab2b6357d141036ea9a12
matthewkuria <mathewkwachira@gmail.com>
2024-09-02 20:05:34 +0300

    Add a title to the vehicle tab

--- src/routes/technology.jsx
+++ src/routes/technology.jsx
@@ -2,6 +2,7 @@ const Technology = () => {
     return (
         <main className="text-white">
             <h1>SPACE LAUNCH 101</h1>
+            <p className="title">LAUNCH VEHICLE</p>
         </main>
     )
 }
