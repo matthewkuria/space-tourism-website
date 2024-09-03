f1d4695c1f48cbeb54f40634aee0020765ebc944
matthewkuria <mathewkwachira@gmail.com>
2024-09-03 11:55:50 +0300

    Fix the technology page

--- src/routes/technology.jsx
+++ src/routes/technology.jsx
@@ -2,16 +2,24 @@ import { useEffect, useState } from "react";
 import "./routes.css";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import imageHurley from "../assets/crew/image-douglas-hurley.png"
-import imageShuttle from "../assets/crew/image-mark-shuttleworth.png"
-import imageGlover from "../assets/crew/image-victor-glover.png"
-import imageAnsari from "../assets/crew/image-anousheh-ansari.png"
+import imageVehicle from "../assets/technology/image-launch-vehicle-landscape.jpg"
+import imageCapsule from "../assets/technology/image-space-capsule-landscape.jpg"
+import imageSpaceport from "../assets/technology/image-spaceport-landscape.jpg"
 
+import imageVehicle2 from "../assets/technology/image-launch-vehicle-portrait.jpg"
+import imageCapsule2 from "../assets/technology/image-space-capsule-portrait.jpg"
+import imageSpaceport2 from "../assets/technology/image-spaceport-portrait.jpg"
+
+const imageMap2 = {
+    'image-launch-vehicle-portrait.jpg': imageVehicle2,
+    'image-space-capsule-portrait.jpg': imageCapsule2,
+    'image-spaceport-portrait.jpg': imageSpaceport2,
+    
+};
 const imageMap = {
-    'image-douglas-hurley.png': imageHurley,
-    'image-mark-shuttleworth.png': imageShuttle,
-    'image-victor-glover.png': imageGlover,
-    'image-anousheh-ansari.png': imageAnsari
+    'image-launch-vehicle-landscape.jpg': imageVehicle,
+    'image-space-capsule-landscape.jpg': imageCapsule,
+    'image-spaceport-landscape.jpg': imageSpaceport,
     
 };
 const Technology = () => {
@@ -22,34 +30,51 @@ const Technology = () => {
     // Fetch data from the JSON file
     fetch('/data.json')
       .then((response) => response.json())
-      .then((data) => setData(data.crew))
+      .then((data) => setData(data.technology))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
     return (
         <main className="text-white  technology">
             <h1>SPACE LAUNCH 101</h1>
-            <p className="title text-5xl">LAUNCH VEHICLE</p>
-            <section className="">
+            <Tabs>
+                <section className="">
                 {Array.isArray(data) && data.map((item, index) => (
-                        <TabPanel key={index}>
-                    <div className='flex flex-col justify-center items-center md:flex-row mt-5 md:mt-0'>                        
-                        <div className="flex flex-col text-white md:w-1/2">
-                            <h1 className="uppercase text-slate-400 text-3xl py-2">{item.role}</h1>
-                            <h1 className='uppercase text-7xl py-2'>{item.name}</h1>
-                            <p>{item.bio}</p>                        
+                    <TabPanel key={index}>
+                        <div className='flex flex-col justify-center items-center md:flex-row mt-5 md:mt-0'>                        
+                            <div className="flex flex-col text-white md:w-1/2">
+                                <h1 className='uppercase text-7xl py-2'>{item.name}</h1>
+                                <p>{item.description}</p>                        
+                            </div>
+                            <div className=''>
+                                <img
+                                    src={imageMap2[item.images.portrait]}
+                                    alt={item.name} 
+                                    className="technology-image hidden md:block"                               
+                                />
+                                <img
+                                    src={imageMap[item.images.landscape]}
+                                    alt={item.name} 
+                                    className="technology-image block md:hidden"                               
+                                />
+                            </div>
                         </div>
-                        <div className=''>
-                            <img
-                                src={imageMap[item.images.png]}
-                                alt={item.name} 
-                                className="crew-image"                               
-                            />
-                        </div>
-                    </div>
                     </TabPanel>
                 ))}
-
+                <TabList className="flex justify-start  uppercase md:my-6 ">
+                    {Array.isArray(data) && data.map((item, index) => (
+                        <Tab key={index}
+                            className="crew-btn"
+                            onClick={() => setValue(index)}
+                            style={{
+                            backgroundColor: index === value ? "white" : "#979797",
+                            }}
+                        >
+                        
+                    </Tab>
+                    ))}
+                </TabList>
             </section>
+            </Tabs>
         </main>
     )
 }
